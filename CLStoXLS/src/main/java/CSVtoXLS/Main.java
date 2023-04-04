84d53c932ac72fcdda32e088feb45a9249332829
Kacper Morawski <morawskik@dc.emersondc.pl>
2023-04-04 12:04:23 +0200

    Renaming variables, adding exceptions

--- CLStoXLS/src/main/java/CSVtoXLS/Main.java
+++ CLStoXLS/src/main/java/CSVtoXLS/Main.java
@@ -4,7 +4,6 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-
 import java.io.*;
 
 public class Main {
@@ -16,7 +15,8 @@ public class Main {
         if(args.length<2 || args.length>4)
         {
             System.out.println(args.length);
-            System.err.println("Zla ilosc parametrow!");
+            System.out.println("Incorrect amount of parameters!");
+            System.out.println("Argument 1: Input Path Argument 2: Output Path Argument 3: Splitter Argument 4: Codding Type");
             System.exit(1);
         }
         else if(args.length==2)
@@ -38,15 +38,17 @@ public class Main {
             encoding = args[3];
 
         }
-        System.out.println("Sciezka wejsciowa: " +path);
-        System.out.println("Sciezka wyjsciowa: " +path1);
-        System.out.println("Typ kodowania: " +encoding);
+        System.out.println("Input path: " +path);
+        System.out.println("Output path: " +path1);
         System.out.println("Splitter: '" +spitter +"'");
+        System.out.println("Codding type: " +encoding);
+        System.out.println();
+
         File folder = new File(path);
         File folder1 = new File(path1);
         if(!folder.exists())
         {
-            folder.mkdir();
+            throw new FileNotFoundException();
         }
         if(!folder1.exists())
         {
@@ -59,49 +61,56 @@ public class Main {
             }
         });
 
-        int ilosc_plikow = 0;
-        int ilosc_lini_w_pliku = 0;
-        int ilosc_lini = 0;
+        int filesCount = 0;
+        int lineCountInFile = 0;
+        int lineCount = 0;
         for(File csvFile : csvFiles)
         {
-            ilosc_plikow++;
+            filesCount++;
+            System.out.println("File opened: " + csvFile.getName());
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), encoding));
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile),encoding));
+                String xlsFileName = csvFile.getName().replaceAll(".csv"
+                        ,".xls");
+                File xlsFile = new File(folder1,xlsFileName);
+                HSSFWorkbook workbook = new HSSFWorkbook();
+                HSSFSheet sheet = workbook.createSheet("Sheet 1");
 
-            String xlsFileName = csvFile.getName().replaceAll(".csv"
-                    ,".xls");
-            File xlsFile = new File(folder1,xlsFileName);
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("Sheet 1");
+                String line;
+                int rowNumber = 0;
 
-            String line;
-            int rowNumber = 0;
+                while((line = reader.readLine())!= null) {
+                    lineCountInFile++;
 
-            while((line = reader.readLine())!= null) {
-                ilosc_lini_w_pliku++;
+                    String[] columns = line.split(spitter);
+                    HSSFRow row = sheet.createRow(rowNumber++);
+                    for (int i = 0; i < columns.length; i++)
+                    {
+                        HSSFCell cell = row.createCell(i);
+                        cell.setCellValue(columns[i]);
 
-                String[] columns = line.split(spitter);
-                HSSFRow row = sheet.createRow(rowNumber++);
-                for (int i = 0; i < columns.length; i++)
-                {
-                    HSSFCell cell = row.createCell(i);
-                    cell.setCellValue(columns[i]);
-
+                    }
                 }
+                lineCount += lineCountInFile;
+
+                OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(xlsFile),encoding);
+                FileOutputStream outputStream = new FileOutputStream(xlsFile);
+                workbook.write(outputStream);
+                writer.close();
+                workbook.close();
+                reader.close();
+
             }
-            ilosc_lini_w_pliku = ilosc_lini_w_pliku - 1;
-            ilosc_lini += ilosc_lini_w_pliku;
+            catch (Exception e)
+            {
+                System.out.println("Error during creating and saving .xls file:"+e.getMessage());
+            }
 
-
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(xlsFile),"WINDOWS-1250");
-            FileOutputStream outputStream = new FileOutputStream(xlsFile);
-            workbook.write(outputStream);
-            workbook.close();
-            writer.close();
-            reader.close();
         }
-        System.out.println("Ilosc plików: " + ilosc_plikow);
-        System.out.println("Ogolna ilosc zczytanych lini: " + ilosc_lini);
+        System.out.println();
+        System.out.println("File count: " + filesCount);
+        System.out.println("Read lines: " + lineCount);
 
     }
 }
