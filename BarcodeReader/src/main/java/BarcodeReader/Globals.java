04c55a6ce59004c23dbe56489c55cdac86ec735d
Kacper Morawski <morawskik@dc.emersondc.pl>
2023-04-27 10:20:27 +0200

    Change version,comment code,add default creation config.ini if it doesnt exist, add polish description

---
+++ BarcodeReader/src/main/java/BarcodeReader/Globals.java
@@ -0,0 +1,23 @@
+package BarcodeReader;
+
+import org.jnativehook.keyboard.NativeKeyEvent;
+
+import javax.swing.*;
+import java.io.File;
+
+public class Globals {
+//    public static int screenshotBind = NativeKeyEvent.VC_F6;
+    public static int screenshotBind;
+    public static int temp;
+    public static int max_number_of_running;
+//    public static int max_number_of_running = 10;
+//    public static int exitBind = NativeKeyEvent.VC_F7;
+    public static int exitBind;
+    public static boolean cropisRunned = false;
+    public static JPanel panel = null;
+    public static JFrame f = new JFrame();
+
+    public static String path = BackgroundApp.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+    public static File ini_path_without_ini = new File(path);
+    public static File configFile = new File(ini_path_without_ini.getParent()+File.separator+"config.ini");
+}
---
+++ BarcodeReader/src/main/java/BarcodeReader/Proper.java
@@ -0,0 +1,83 @@
+package BarcodeReader;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class Proper extends Properties {
+    public static Properties prop = null;
+    public static InputStream inputStream;
+
+
+
+    public Proper() throws IOException {
+        //if config doesnt exist create one with default keybinds and max amount of run function
+        if(!Globals.configFile.exists())
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(Globals.configFile));
+            StringBuilder sb = new StringBuilder();
+            sb.append("SCREENSHOT_BIND=64\nEXIT_BIND=65\nMAX_TIMES_RUNNED_APP=10");
+            writer.write(sb.toString());
+            writer.close();
+        }
+        inputStream = Files.newInputStream(Paths.get(Globals.ini_path_without_ini.getParent() + File.separator + "config.ini"));
+    }
+
+    public boolean init() {
+        prop = new Properties();
+        try {
+            prop.load(inputStream);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getProper(String propertyName) {
+        String temp;
+        temp = prop.getProperty(propertyName);
+        System.out.println(propertyName + ": " + temp);
+        if (temp == null) {
+            System.err.println("Couldn't read parameter " + propertyName + " from .ini file");
+        }
+        return temp;
+    }
+
+    public static boolean loadProperties() {
+        try {
+            Globals.screenshotBind = Integer.parseInt(getProper("SCREENSHOT_BIND"));
+            Globals.exitBind = Integer.parseInt(getProper("EXIT_BIND"));
+            Globals.max_number_of_running = Integer.parseInt(getProper("MAX_TIMES_RUNNED_APP"));
+            return true;
+        } catch (Exception e) {
+            System.out.println("Exception during loading files from .ini file");
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public static void savePropertoIni(String ini_text, int keyBindValue) throws IOException, URISyntaxException {
+        BufferedReader reader = new BufferedReader(new FileReader(Globals.configFile));
+        System.out.println(Globals.configFile);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        // Read through the input stream, looking for the line to overwrite
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith(ini_text)) {
+                // Replace the line with the new line
+                sb.append(ini_text + "=" + keyBindValue).append(System.lineSeparator());
+            } else {
+                // Keep the existing line
+                sb.append(line).append(System.lineSeparator());
+            }
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(Globals.configFile));
+        writer.write(sb.toString());
+        writer.close();
+
+    }
+
+
+}
